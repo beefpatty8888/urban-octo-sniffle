17245c1ef7283e251245565131dd28850091131b
beefpatty8888 <16585824+beefpatty8888@users.noreply.github.com>
2018-06-02 17:21:13 -0500

    Add classes for listing and creating droplets. Currently, only the listing of droplets actually functions.
    Add also the proper parameters with logging and some error
    handling.

--- createDigitalOceanDroplet.py
+++ createDigitalOceanDroplet.py
@@ -7,6 +7,28 @@ import logging
 
 import os
 
+import sys
+
+class createDroplet:
+   def __init__(self, dropletSize, token):
+     logger.debug ("Creating droplet of size "+dropletSize)
+
+
+class listDroplets:
+   def __init__(self, token):
+     logger.debug ("Listing droplets")
+     self.getList(token)
+
+   def getList(self,token):
+     manager = digitalocean.Manager(token=token)
+     myDroplets = manager.get_all_droplets()
+     logger.debug(myDroplets)
+ 
+     # see https://developers.digitalocean.com/documentation/v2/#list-all-droplets
+     # for all attributes in the response.
+     for droplet in myDroplets:
+        logger.debug(str(droplet.id) + ";" + droplet.name + ";" + droplet.image["distribution"] + ";" + str(droplet.networks["v4"]))
+
 def main():
    
    #http://docs.python-guide.org/en/latest/writing/logging/
@@ -17,27 +39,47 @@ def main():
    streamHandler.setFormatter(formatter)
    logger.addHandler(streamHandler)
 
-   fileHandler = logging.FileHandler("instance-creation.log")
+   fileHandler = logging.FileHandler("../instance-creation.log")
    fileHandler.setFormatter(formatter)
    logger.addHandler(fileHandler)
    logger.setLevel(logging.DEBUG) 
 
-   logger.debug ("Starting instance creation")
+   logger.debug ("Starting script")
 
    parser = argparse.ArgumentParser()
+   parser.add_argument ("--create", help="Create a Digital Ocean droplet", action="store_true")
    parser.add_argument("--size", type=str, help="Digital Ocean droplet size")
+   parser.add_argument ("--list", help="List Digital Ocean droplets", action="store_true")
    args = parser.parse_args()
+
+   # https://stackoverflow.com/questions/10698468/argparse-check-if-any-arguments-have-been-passed
+   if not len (sys.argv) > 1:
+      logger.error ("Please specify a --list or --create flag.")
 
    # DO_API_TOKEN enviroment variable is used to be consistent
    # with ansible's digital_ocean module
    # http://docs.ansible.com/ansible/latest/modules/digital_ocean_module.html
-   if args.size != None and os.getenv ("DO_API_TOKEN") != None:
-     logger.debug ("Using DO Token " + os.getenv ("DO_API_TOKEN"))
-     logger.debug ("Creating droplet of size " + args.size)
-      
-   elif os.getenv ("DO_API_TOKEN") == None:
-     logger.error ("System environment variable DO_API_TOKEN must be specified.")
 
+   if os.getenv ("DO_API_TOKEN") == None:
+      logger.error ("System environment variable DO_API_TOKEN must be specified.")
+      sys.exit()
+   else:
+      logger.debug("Using token "+ os.getenv ("DO_API_TOKEN"))
+
+   if args.create == True and args.list == True:
+     logger.error ("Please specify only the --create or --list flag but not both.")
+     sys.exit()
+
+   if args.create == True:
+     if args.size != None:
+       createDroplet (args.size, os.getenv("DO_API_TOKEN"))
+
+     elif args.size == None:
+       logger.error ("The slug for the size of the instance must be specified.")
+       logger.error ("See https://developers.digitalocean.com/documentation/changelog/api-v2/new-size-slugs-for-droplet-plan-changes/")
+
+   if args.list == True:
+     listDroplets(os.getenv("DO_API_TOKEN"))
 
 if __name__ == "__main__":
    main()
